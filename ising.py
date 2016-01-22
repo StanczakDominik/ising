70d0b509244b97ce0fa4a0caca10f52424caca39
Dominik Stańczak <stanczakdominik@gmail.com>
2016-01-22 11:55:12 +0100

    Trying to figure out why magnetization always turns out negative. Added file management

--- ising.py
+++ ising.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from time import time
 
 # random.seed(1)
-def ising(N, NT, T, plotting=False, show=False, anim=False):
+def ising(N, NT, T, plotting=False, show=False, anim=False, restart=True):
 
     start_time = time()
 
@@ -21,8 +21,6 @@ def ising(N, NT, T, plotting=False, show=False, anim=False):
     history = np.empty((NT+1,saved_parameters))
     if anim:
         Nsnapshots = int(np.sqrt(NT))
-        if Nsnapshots < 10:
-            Nsnapshots=NT
         snapshot_history = np.empty((Nsnapshots, N,N), int)
 
     def Energy(spins):
@@ -38,18 +36,18 @@ def ising(N, NT, T, plotting=False, show=False, anim=False):
     def FlipSpin(spins, E, M):
         x, y = random.randint(1,N+2, 2)
         test = spins[x,y]
-        deltaE=J*2*test
+        deltaE=-J*2*test
         accepted = 0
         if(random.random() < np.exp(-beta*deltaE)):
             E += deltaE
             M -= 2*test
+            accepted = test
             spins[x,y] *= -1
-            accepted = 1
         return E, M, accepted
     def ViewSystem(title):
         print(title)
         print("Energy: %d\tMagnetization: %d\tN: %d\tT: %.2f" %(E,M,N,T))
-        print(spins[1:-1])
+        print(spins[1:-1,1:-1])
 
     def ShowSpins(spins):
         plot = plt.imshow(spins)
@@ -60,8 +58,11 @@ def ising(N, NT, T, plotting=False, show=False, anim=False):
     spins=np.ones([Noffset,Noffset], int)
     spins[:,0] = spins[:,-1] = spins[0,:] = spins[-1, :] = 0
     # spins[1:-1,1:-1:2,] = -1
-    spins[1:-1, 1:-1] = random.randint(0,2, (N,N))*2-1
-
+    if restart:
+        spins[1:-1, 1:-1] = random.randint(0,2, (N,N))*2-1
+        np.save("data/N%d_T%.2f_start"%(N,T), spins)
+    else:
+        spins=np.load("N%d_T%.2f_start.npy"%(N,T))
     E, M = Energy(spins), Mag(spins)
     parameters = E, M, -1
     history[0] = parameters
@@ -72,15 +73,18 @@ def ising(N, NT, T, plotting=False, show=False, anim=False):
         history[i+1]= parameters
         if anim and i%(Nsnapshots)==0:
             snapshot_history[int((i/NT)*Nsnapshots)]=spins[1:-1, 1:-1]
+
+    np.save("data/N%d_T%.2f_finish"%(N,T), spins)
     ViewSystem("Finished")
     energies = history[:,0]
     magnetization = history[:,1]
     acceptance = history[:,2]
-    print("Acceptance ratio: %f" %np.mean(acceptance))
+    print("Average spin from which transition was accepted: %f" %np.mean(acceptance))
     print("Runtime: %f" % (time()-start_time))
 
     def plot():
-        fig, (ax_energy, ax_magnet) = plt.subplots(2, sharex=True, sharey=False,figsize=(15,7) )
+        fig, (ax_energy, ax_magnet, ax_acceptance) = plt.subplots(3, sharex=True, sharey=False,figsize=(15,12) )
+        plt.title("Energy: %d Magnetization: %d N: %d T: %.2f" %(E,M,N,T))
 
         ax_energy.plot(range(NT+1),energies, "b-", label="Energy")
         ax_energy.plot(range(NT+1), np.ones(NT+1)*max_e, "b--", label="Max energy")
@@ -92,14 +96,16 @@ def ising(N, NT, T, plotting=False, show=False, anim=False):
         ax_magnet.plot(range(NT+1), np.ones(NT+1)*max_m, "g--", label="Max magnetization")
         ax_magnet.plot(range(NT+1), -np.ones(NT+1)*max_m, "g--")
         ax_magnet.set_ylabel("Magnetization")
+
+        ax_acceptance.plot(range(NT+1),acceptance, "g-", label="Magnetization")
+        ax_acceptance.set_ylabel("Accepted change from spin:")
         # ax_magnet.legend()
 
 
-        plt.title("Energy: %d Magnetization: %d N: %d T: %.2f" %(E,M,N,T))
         plt.xlabel("Time")
 
 
-        title_string = "N%d_T%.2f.png"%(N,T)
+        title_string = "plots/N%d_T%.2f.png"%(N,T)
         plt.savefig(title_string)
         if(show):
             plt.show()
@@ -109,7 +115,7 @@ def ising(N, NT, T, plotting=False, show=False, anim=False):
         plot()
 
     def animate():
-        fig = plt.figure(figsize=(15,7))
+        fig=plt.figure()
         plot = plt.imshow(snapshot_history[0], interpolation="nearest")
         plot.set_cmap('Greys_r')
         plt.colorbar()
@@ -120,8 +126,9 @@ def ising(N, NT, T, plotting=False, show=False, anim=False):
             plot.set_array(snapshot_history[i//Nsnapshots])
             return plot, title
 
-        ani = matplotlib.animation.FuncAnimation(fig, update_fig, np.arange(0,NT-1,NT//Nsnapshots), interval=30, repeat=True)
+        ani = matplotlib.animation.FuncAnimation(fig, update_fig, np.arange(0,NT-1,NT//Nsnapshots), interval=30, blit=False, repeat=True)
+        ani.save('plots/cabbage.mp4', writer='mencoder', fps=30)
         plt.show()
     if(anim):
         animate()
-ising(512,5e6,0.1, anim=True)
+ising(2,5,2, plotting=True)#, show=True, anim=True)
